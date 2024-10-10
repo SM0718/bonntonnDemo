b462d2813d768ab1f83cc45dfd26523bdb2386bd
Sagnik Majumder <clashofclanssagnik@gmail.com>
2024-10-10 17:54:04 +0530

    FIxed Whatsapp Button

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -25,7 +25,7 @@ function Home() {
 
             <div className='w-full h-screen bg-cover bg-top bg-no-repeat flex justify-start items-end relative bg-fixed
         ' style={{backgroundImage: `url('./mainBanner.webp')`}}>
-            <div className="w-3/4 mx-auto m-16 flex flex-col gap-4 items-center z-40 relative before:content-[''] before:absolute before:w-full before:h-full before:bg-black/50 before:py-6 before:rounded-xl p-2">
+            <div className="w-3/4 mx-auto m-16 flex flex-col gap-4 items-center relative before:content-[''] before:absolute before:w-full before:h-full before:bg-black/50 before:py-6 before:rounded-xl before:z-30 p-2">
                 <h2 className='headings text-center text-2xl lg:text-5xl font-medium text-white px-1 md:px-4 b z-50 py-4'><TypeAnimations /></h2>
                 <p className='hidden text-center md:flex text-base md:text-xl text-white px-4 b z-50'>
                 Luxury gift boxes that are thoughtfully curated to celebrate moments, create memories, and deepen connections.</p>
