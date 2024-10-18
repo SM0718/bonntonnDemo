cd131ffbaa669526fa0bc63a410e24e78423c2f1
Sagnik Majumder <clashofclanssagnik@gmail.com>
2024-10-18 18:47:12 +0530

    Added Banner For Phone

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -26,8 +26,18 @@ function Home() {
   return (
     <div className='flex flex-col items-center justify-center relative'>
 
-            <div className='w-full h-screen bg-cover bg-center bg-no-repeat p-8 flex justify-start items-end relative bg-fixed
+            <div className='w-full h-screen bg-cover bg-center bg-no-repeat p-8 hidden md:flex justify-start items-end relative bg-fixed
         ' style={{backgroundImage: `url('./mainBanner.webp')`}}>
+            <div className="w-3/4 mx-auto m-16 flex flex-col gap-4 items-center relative before:content-[''] before:absolute before:w-full before:h-full before:bg-black/50 before:py-6 before:rounded-xl before:z-30 p-2">
+                <h2 className='headings text-center text-2xl lg:text-5xl font-medium text-white px-1 md:px-4 b z-40 py-4'><TypeAnimations /></h2>
+                <p className='hidden text-center md:flex text-base md:text-xl text-white px-4 b z-40'>
+                Luxury gift boxes that are thoughtfully curated to celebrate moments, create memories, and deepen connections.</p>
+                <p className='md:hidden flex text-white text-center text-base px-1 b z-40'>Luxury gift boxes that are thoughtfully curated to celebrate moments, create memories, and deepen connections.</p>
+              </div>
+          </div>
+
+          <div className='w-full h-screen bg-cover bg-top bg-no-repeat p-8 md:hidden flex justify-start items-end relative bg-fixed
+        ' style={{backgroundImage: `url('./mobileBanner.webp')`}}>
             <div className="w-3/4 mx-auto m-16 flex flex-col gap-4 items-center relative before:content-[''] before:absolute before:w-full before:h-full before:bg-black/50 before:py-6 before:rounded-xl before:z-30 p-2">
                 <h2 className='headings text-center text-2xl lg:text-5xl font-medium text-white px-1 md:px-4 b z-40 py-4'><TypeAnimations /></h2>
                 <p className='hidden text-center md:flex text-base md:text-xl text-white px-4 b z-40'>
